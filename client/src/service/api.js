f8004dfba0410cc3f6f323d9ac0a5eda571a0a51
Dipesh <dipeshrjk27@gmail.com>
2024-04-17 08:02:54 +0530

    added another cors middleware

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -7,6 +7,17 @@ export const uploadFile=async(data)=>{
         console.log(response.data);
         return response.data;
     } catch (error) {
-        console.log(`error while calling the API ${error.message}`);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            console.log(`Server responded with status ${error.response.status}`);
+            console.log(error.response.data);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(`No response received from server: ${error.request}`);
+        } else {
+            // Something else happened while setting up the request
+            console.log(`Error setting up the request: ${error.message}`);
+        }
     }
 }
