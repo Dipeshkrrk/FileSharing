1f02535f2b11926f7ce07c4a8a515a07c0f3f1ac
Dipesh <dipeshrjk27@gmail.com>
2024-04-17 07:52:11 +0530

    added another cors middleware

--- server/index.js
+++ server/index.js
@@ -3,19 +3,19 @@ import router from "./routes/routes.js";
 import cors from 'cors';
 import dbConnection from "./database/db.js";
 const app=express();
-// app.use(cors(
-//     {origin:['https://filesharing-five.vercel.app'],
-//     methods:['GET','POST'],
-//     credentials:true
-// }
-// ));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://filesharing-five.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+app.use(cors(
+    {origin:['https://filesharing-five.vercel.app'],
+    methods:['GET','POST'],
+    credentials:true
+}
+));
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://filesharing-five.vercel.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 app.use('/',router);
 
 const port=8000;
