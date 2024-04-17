ddae44285252bddffad3d5d3265ef3af240b3d39
Dipesh <dipeshrjk27@gmail.com>
2024-04-17 07:56:47 +0530

    added another cors middleware

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -4,6 +4,7 @@ axios.defaults.withCredentials=true;
 export const uploadFile=async(data)=>{
     try {
         let response= await axios.post(`${API_URL}/upload`,data);
+        console.log(response.data);
         return response.data;
     } catch (error) {
         console.log(`error while calling the API ${error.message}`);
